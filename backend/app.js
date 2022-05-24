cd81f0356ed0dfaea50c814e085bf782cd5150c0
Arnaud Defrancq <arnaud.defrancq@laposte.net>
2022-05-24 13:33:36 +0200

    initialisation du projet

---
+++ backend/app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config({ path: "./config/.env" });
+const rateLimit = require("express-rate-limit");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many request from this IP",
+});
+
+app.use(limiter);
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Hello from the express server",
+  });
+});
+
+// Route user
+
+module.exports = app;
