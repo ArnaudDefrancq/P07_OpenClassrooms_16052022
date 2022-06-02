2e8908113a7d76b936cf79a90eaab447dae67d13
Arnaud Defrancq <arnaud.defrancq@laposte.net>
2022-06-02 16:21:09 +0200

    connection backend / frontend partie login

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import "./Login.scss";
+import {POST} from "../../components/api/axios";
 
 const Login = () => {
+
+    const [userLogin, setUserLogin] = useState({
+        email: "",
+        password: "",
+      });
+
+    const login = async(e) => {
+        e.preventDefault();
+        try {
+            await POST("api/auth/login", userLogin)
+            .then(res => console.log(res, 'connecter'))
+            .catch (err => console.log(err.message))
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
+
     return (
         <div className='form-container'>
             <h1 className='title'>Connection</h1>
-            <form>
-                <input type="email" placeholder='Adresse mail' required/>
+            <form onSubmit={login}>
+                <input 
+                type="email" 
+                placeholder='Adresse mail' 
+                onChange={(e) =>
+                    setUserLogin({
+                      ...userLogin,
+                      email: e.target.value,
+                    })
+                }
+                value={userLogin.email}
+                required/>
 
-                <input type="text" placeholder='Mot de passe' required/>
+                <input 
+                type="password" 
+                placeholder='Mot de passe' 
+                onChange={(e) =>
+                    setUserLogin({
+                      ...userLogin,
+                      password: e.target.value,
+                    })
+                }
+                value={userLogin.password}
+                required/>
 
                 <button type="submit" value="connection">Connection</button>
             </form>
