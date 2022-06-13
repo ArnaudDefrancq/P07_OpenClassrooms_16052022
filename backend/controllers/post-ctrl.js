443551f9b49df4ad851d4cb6b300af69c6725965
Arnaud Defrancq <arnaud.defrancq@laposte.net>
2022-06-13 20:52:58 +0200

    Ajout des controllers pour les commentaires

--- backend/controllers/post-ctrl.js
+++ backend/controllers/post-ctrl.js
@@ -1,12 +1,13 @@
 const post = require("../models/Post.model");
-const user = require("../models/User.model");
 const ObjectID = require("mongoose").Types.ObjectId;
 
 exports.findAllPost = (req, res, next) => {
-  post.find((err, docs) => {
-    if (!err) res.send(docs);
-    else console.log("error to get data" + err);
-  });
+  post
+    .find((err, docs) => {
+      if (!err) res.send(docs);
+      else console.log("error to get data" + err);
+    })
+    .sort({ createdAt: -1 });
 };
 
 exports.createPost = async (req, res, next) => {
@@ -99,5 +100,81 @@ exports.unlikePost = async (req, res, next) => {
     );
   } catch (err) {
     res.status(400).send(err);
+  }
+};
+
+exports.commentPost = (req, res, next) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unknown : " + req.params.id);
+
+  try {
+    return post.findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: {
+          comments: {
+            commenterId: req.body.commenterId,
+            commenterPseudo: req.body.commenterPseudo,
+            text: req.body.text,
+            timestamp: new Date().getTime(),
+          },
+        },
+      },
+      { new: true },
+      (err, docs) => {
+        if (!err) return res.send(docs);
+        else return res.status(400).send(err);
+      }
+    );
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+};
+
+exports.editCommentPost = (req, res, next) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unknown : " + req.params.id);
+
+  try {
+    return post.findById(req.params.id, (err, docs) => {
+      const theComment = docs.comments.find((comment) =>
+        comment._id.equals(req.body.commentId)
+      );
+
+      if (!theComment) return res.status(404).send("Comment not found");
+      theComment.text = req.body.text;
+
+      return docs.save((err) => {
+        if (!err) return res.status(200).send(docs);
+        return res.status(500).send(err);
+      });
+    });
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
+
+exports.deleteCommentPost = (req, res, next) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unknown : " + req.params.id);
+
+  try {
+    return post.findByIdAndUpdate(
+      req.params.id,
+      {
+        $pull: {
+          comments: {
+            _id: req.body.commentId,
+          },
+        },
+      },
+      { new: true },
+      (err, docs) => {
+        if (!err) return res.send(docs);
+        else return res.status(400).send(err);
+      }
+    );
+  } catch (err) {
+    return res.status(400).send(err);
   }
 };
--- backend/routes/post-route.js
+++ backend/routes/post-route.js
@@ -4,11 +4,17 @@ const postCtrl = require("../controllers/post-ctrl.js");
 const multer = require("../middleware/multer-config");
 const auth = require("../middleware/auth-config");
 
+//Route des posts
 router.post("/", postCtrl.createPost);
 router.get("/", postCtrl.findAllPost);
 router.put("/:id", multer, postCtrl.modifyPost);
 router.delete("/:id", postCtrl.deletePost);
 router.patch("/like-post/:id", postCtrl.likePost);
 router.patch("/unlike-post/:id", postCtrl.unlikePost);
+
+// Route des  commentaires
+router.patch("/comment-post/:id", postCtrl.commentPost);
+router.patch("/edit-comment-post/:id", postCtrl.editCommentPost);
+router.patch("/delete-comment-post/:id", postCtrl.deleteCommentPost);
 
 module.exports = router;
