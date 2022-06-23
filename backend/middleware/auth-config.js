2ef2e5758169e2ea94ee2a5308dff84c084e731b
Arnaud Defrancq <arnaud.defrancq@laposte.net>
2022-06-23 09:55:10 +0200

    Rectification du middleware auth

--- backend/middleware/auth-config.js
+++ backend/middleware/auth-config.js
@@ -1,35 +1,18 @@
 const jwt = require("jsonwebtoken");
 
-// const extractBearer = (auth) => {
-//   if (typeof auth !== "string") {
-//     return false;
-//   }
-
-//   const matches = auth.match(/(bearer)\s+(\S+)/i);
-
-//   return matches && matches[2];
-// };
-
 const checkToken = (req, res, next) => {
-  const token = req.cookies.jwt;
-  console.log("AUTH", req.headers);
+  const token = req.headers.authorization.split(" ")[1];
   console.log("TOKEN,", token);
 
   if (!token) {
     return res.status(401).json({ message: "0 token" });
   }
 
-  jwt.verify(token, process.env.TOKEN, (err, decoded) => {
-    const userIdToken = decoded.userId;
-    req.auth = { userId: userIdToken };
-    req.auth;
-    console.log(userIdToken);
-    if (err) {
-      return res.status(401).json({ message: "mauvais token" });
-    }
+  const decodedToken = jwt.verify(token, process.env.TOKEN);
 
-    next();
-  });
+  const userId = decodedToken.userId;
+  req.auth = { userId };
+  next();
 };
 
 module.exports = checkToken;
