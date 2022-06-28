ce2259d882e01e6ac63ed53e2589eb5c821300e9
Arnaud Defrancq <arnaud.defrancq@laposte.net>
2022-06-28 13:35:29 +0200

    Ajout de la fonction afficher et poster un commentaire

--- frontend/src/components/Home/Newfeeds/Newfeeds.jsx
+++ frontend/src/components/Home/Newfeeds/Newfeeds.jsx
@@ -4,14 +4,17 @@ import { useState } from 'react';
 import axios from "axios";
 import Moment from "react-moment";
 import 'moment/locale/fr'
+import CreateCom from '../CreateCom/CreateCom';
 
 const Newfeeds = () => {
 
     const [loadPost, setLoadPost] = useState([]);
     const [loadUser, setLoadUser] = useState([]);
+    const [loadCom, setLoadCom] = useState([]);
+    // const [comValue, setComValue] = useState([]);
 
     const user = document.cookie.split('=');
-    console.log(user[1]);
+    // console.log(user[1]);
 
     const config = {
         headers: {
@@ -31,6 +34,28 @@ const Newfeeds = () => {
         .catch((err) => console.log(err))
 
     }, [])
+
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_API_URL}api/com/`)
+        .then((res) => setLoadCom(res.data))
+        .catch(err => console.log(err))
+    }, [])
+
+
+
+    // const onSubmit = async (e) => {
+    //     e.preventDefault()
+
+    //     const formData = new FormData()
+    //     formData.append('content', loadCom)
+    //     formData.append('PostId', loadPost.id)
+
+    //     console.log(formData);
+        
+    //     await axios.post(`${process.env.REACT_APP_API_URL}api/com/`, formData, config)
+    //     .then((res) => console.log(res))
+    //     .catch(err => console.log(err))
+    // }
 
     return (
         <div className='feed-container'>
@@ -58,6 +83,21 @@ const Newfeeds = () => {
                                 {
                                     post.attachment ? <img src={post.attachment} alt="user" /> : null
                                 }
+                            </div>
+
+                            <div>
+                                <ul>
+                                    {
+                                        loadCom.map((data) => {
+                                            if (data.PostId === post.id) {
+                                                return <li key={data.id}>{data.content}</li>
+                                            } else {
+                                                return null
+                                            }
+                                        })
+                                    }
+                                </ul>
+                                <CreateCom post={post}/>
                             </div>
                         </li>
                     })
