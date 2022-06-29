b973cc6519c502c96b4154c77be5f777a618ff7b
Arnaud Defrancq <arnaud.defrancq@laposte.net>
2022-06-29 20:07:19 +0200

    suppression des log

--- backend/middleware/auth-config.js
+++ backend/middleware/auth-config.js
@@ -2,21 +2,17 @@ const jwt = require("jsonwebtoken");
 
 const checkToken = (req, res, next) => {
   const token = req.headers.authorization.split(" ")[1];
-  console.log("TOKEN,", token);
 
   if (!token) {
     return res.status(401).json({ message: "0 token" });
   }
 
   const decodedToken = jwt.verify(token, process.env.TOKEN);
-  console.log(decodedToken);
 
   const userId = decodedToken.userId;
   const userPseudo = decodedToken.userPseudo;
 
   req.auth = { userId: userId, userPseudo: userPseudo };
-
-  console.log(req.auth);
 
   next();
 };
