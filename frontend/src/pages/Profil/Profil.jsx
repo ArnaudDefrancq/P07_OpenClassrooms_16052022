8d032c970f51e1b8779192079cdf901bb2697bd8
Arnaud Defrancq <arnaud.defrancq@laposte.net>
2022-07-13 11:26:47 +0200

    Ajout de la page pour voir le profil de l'utilisateur

--- frontend/src/pages/Profil/Profil.jsx
+++ frontend/src/pages/Profil/Profil.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import UserProfil from '../../components/ProfilPage/User-profil';
 
 const Profil = () => {
     return (
         <div>
-            
+            <UserProfil />
         </div>
     );
 };
