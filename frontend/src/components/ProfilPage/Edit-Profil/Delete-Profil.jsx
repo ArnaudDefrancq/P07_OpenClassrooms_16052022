a750cb7f37171d2d82bdba066e3a7f7002675e6b
Arnaud Defrancq <arnaud.defrancq@laposte.net>
2022-07-18 11:27:29 +0200

    Ajout de la logique pour delete un profil

--- frontend/src/components/ProfilPage/Edit-Profil/Delete-Profil.jsx
+++ frontend/src/components/ProfilPage/Edit-Profil/Delete-Profil.jsx
@@ -1,9 +1,30 @@
+import axios from "axios";
 import React from "react";
 
-const DeleteProfil = () => {
+const DeleteProfil = ({ user }) => {
+  const userToken = document.cookie;
+
+  const jwt = userToken.split("=");
+  const config = {
+    headers: {
+      authorization: `bearer ${jwt[1]}`,
+    },
+  };
+  const disableProfil = (e) => {
+    if (window.confirm(`Voulez-vous désactiver ce compte?`)) {
+      e.preventDefault();
+      axios
+        .delete(`${process.env.REACT_APP_API_URL}api/post/${user.id}`, config)
+        .then(() => console.log("Compte effacé"))
+        .catch((err) => console.log(err));
+      window.location = "/";
+    } else {
+      console.log("Bonjour");
+    }
+  };
   return (
     <div>
-      <button>Supprimer le compte</button>
+      <button onClick={disableProfil}>Supprimer le compte</button>
     </div>
   );
 };
